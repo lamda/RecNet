deef56e2df264d66f068f22484f43f17a1f84a61
Daniel Lamprecht <daniel.lamprecht@gmx.at>
2016-02-14 23:06:34 +0100

    fixed a bug for the alluvial plots

--- plots/alluvial/crop.py
+++ plots/alluvial/crop.py
@@ -4,6 +4,7 @@ from __future__ import division, print_function, unicode_literals
 
 import os
 import pdb
+import shutil
 
 if __name__ == '__main__':
     files = [f for f in os.listdir('.') if f.endswith('.html')]
@@ -12,17 +13,21 @@ if __name__ == '__main__':
         (f, f.rsplit('.', 1)[0] + '_full.pdf', f.rsplit('.', 1)[0] + '.pdf') 
         for f in files
     ]
-    files = files[:1]
+    # files = files[:1]
     
-    print('converting to pdf')
+    print('converting to pdf...')
     for f in files:
         cmd = '"C:\\Program Files\\wkhtmltopdf\\bin\wkhtmltopdf.exe" ' +\
               f[0] + ' ' + f[1]
         os.system(cmd)
     
-    print('cropping pdf')
+    print('cropping pdf...')
     for f in files:
-        cmd = 'pdfcrop ' + f[1] + ' ' + f[2]
+        cmd = 'pdfcrop --margins 15 ' + f[1] + ' ' + f[2]
         os.system(cmd)
+        
+    print('copying pdfs...')
+    for f in files:
+        shutil.copy(f[2], 'D:\\PhD\\Dropbox\\paper_recnet\\figures')
         
     print('done')
